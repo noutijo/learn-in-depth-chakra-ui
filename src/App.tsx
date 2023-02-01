dee5c3a95438e00cb21f63d64b25e823e3303db3
Oreol Noumodong <oreolnoumodong@gmail.com>
2023-02-01 12:14:12 +0100

    start learning 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css"
 import { Box } from "@chakra-ui/react"
-import { Header  } from "./components/Header"
-import { Pricing } from "./components/Pricing"
-import { Features } from "./components/Features"
 
 export default function App() {
   return (
     <div className="App">
       <Box>
-        <Header />  
-        <Pricing />  
-        <Features />  
+        yo
       </Box>
     </div>
   )
